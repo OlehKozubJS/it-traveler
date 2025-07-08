41eb3bad5803e74e3be086832c6b6480b7ded89a
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 18:13:17 +0300

    Update HomepageView.vue

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -5,7 +5,7 @@ import { IButton } from '../components/IButton'
 </script>
 
 <template>
-  <main class="flex h-screen">
+  <main class="flex h-screen bg-primary">
     <section class="flex-1 flex justify-center items-center px-5">
       <div class="text-white text-center">
         <img class="inline mb-6" src="../assets/img/map-pin.svg" alt="" />
