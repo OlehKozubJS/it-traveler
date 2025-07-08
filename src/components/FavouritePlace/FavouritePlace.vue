ed92be0309714afd9d438684117390529920baaa
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 17:01:22 +0300

    Update FavouritePlace.vue

--- src/components/FavouritePlace/FavouritePlace.vue
+++ src/components/FavouritePlace/FavouritePlace.vue
@@ -4,8 +4,16 @@
   <section section class="text-[#939393] mb-6 last:mb-0">
     <div class="flex gap-4">
       <img class="w-[76px] h-[76px] shrink-0" src="" alt="" />
-      <div class=""></div>
-      <p class=""></p>
+      <div>
+        <div class="flex justify-between items-center mb-2">
+          <h2></h2>
+          <div>
+            <button></button>
+            <button></button>
+          </div>
+        </div>
+        <p class=""></p>
+      </div>
     </div>
     <div class="h-[1px] w-full bg-[#ececec]"></div>
   </section>
