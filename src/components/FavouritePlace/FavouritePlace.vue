d2eb173c29002130c1b4961e175fc3f4312ce424
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 16:55:41 +0300

    Update FavouritePlace.vue

--- src/components/FavouritePlace/FavouritePlace.vue
+++ src/components/FavouritePlace/FavouritePlace.vue
@@ -1,5 +1,8 @@
 <script setup></script>
 
 <template>
-  <section section class="text-[#939393] mb-6 last:mb-0"><div class="flex gap-4"></div></section>
+  <section section class="text-[#939393] mb-6 last:mb-0">
+    <div class="flex gap-4"></div>
+    <div class="h-[1px] w-full bg-[#ececec]"></div>
+  </section>
 </template>
