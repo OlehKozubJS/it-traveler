21a03c7be0483eaa580f650310689e181d2d7419
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 18:24:32 +0300

    Update DeleteIcon.vue

--- src/components/FavouritePlace/DeleteIcon.vue
+++ src/components/FavouritePlace/DeleteIcon.vue
@@ -3,7 +3,6 @@
     <path
       d="M6 2H10"
       stroke="currentColor"
-      stroke-opacity="0.4"
       stroke-width="1.3"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -11,7 +10,6 @@
     <path
       d="M2 4H14"
       stroke="currentColor"
-      stroke-opacity="0.4"
       stroke-width="1.3"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -19,7 +17,6 @@
     <path
       d="M3.3335 4L3.73409 10.0089C3.80846 11.1245 3.84565 11.6823 4.02795 12.1304C4.35894 12.9441 5.02988 13.5718 5.86373 13.8479C6.32301 14 6.88206 14 8.00016 14V14C9.11826 14 9.67731 14 10.1366 13.8479C10.9704 13.5718 11.6414 12.9441 11.9724 12.1304C12.1547 11.6823 12.1919 11.1245 12.2662 10.0089L12.6668 4"
       stroke="currentColor"
-      stroke-opacity="0.4"
       stroke-width="1.3"
       stroke-linecap="round"
       stroke-linejoin="round"
