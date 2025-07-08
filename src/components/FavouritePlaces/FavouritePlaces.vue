957392c7e855bf3375921c1e13d482945eaafff9
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 20:10:05 +0300

    Update FavouritePlaces.vue

--- src/components/FavouritePlaces/FavouritePlaces.vue
+++ src/components/FavouritePlaces/FavouritePlaces.vue
@@ -25,12 +25,15 @@ const changeSuperUserName = () => {
 
 <template>
   <div class="px-6">
-    <div class="text-grey">User name {{ user.name }}</div>
     <div class="text-grey mb-4">Додані маркери {{ counter }}</div>
     <slot name="label"></slot>
     <slot name="list"></slot>
     <FavouritePlace :key="number" v-for="number in 4" />
     <IButton class="w-full mt-10" @click="increment">Додати маркер</IButton>
+
+    <div class="text-grey">User name {{ user.name }}</div>
     <IButton class="w-full mt-10" @click="changeUserName">Change user name</IButton>
+    <div class="text-grey">Super user name {{ superUser.name }}</div>
+    <IButton class="w-full mt-10" @click="changeSuperUserName">Change super user name</IButton>
   </div>
 </template>
