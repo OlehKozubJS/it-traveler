e2245a341390bb07c8fae12dec7682670ca1d28c
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 21:38:35 +0300

    Update FavouritePlaces.vue

--- src/components/FavouritePlaces/FavouritePlaces.vue
+++ src/components/FavouritePlaces/FavouritePlaces.vue
@@ -1,6 +1,7 @@
 <script setup>
 import FavouritePlace from '../FavouritePlace/FavouritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+import IInput from '../IInput/IInput.vue'
 
 import { ref } from 'vue'
 
@@ -13,9 +14,8 @@ const changeButtonVariant = () => {
 
 <template>
   <div class="px-6">
+    <IInput />
     <div class="text-grey mb-4">Додані маркери {{ buttonVariant }}</div>
-    <slot name="label"></slot>
-    <slot name="list"></slot>
     <FavouritePlace :key="number" v-for="number in 4" />
     <IButton class="w-full mt-10" @click="changeButtonVariant" :variant="buttonVariant"
       >Додати маркер</IButton
