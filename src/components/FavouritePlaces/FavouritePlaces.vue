b03694eb29ab6f17d4be7b71a660357a62c73083
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 19:55:52 +0300

    Update FavouritePlaces.vue

--- src/components/FavouritePlaces/FavouritePlaces.vue
+++ src/components/FavouritePlaces/FavouritePlaces.vue
@@ -13,6 +13,7 @@ const increment = () => {
 
 <template>
   <div class="px-6">
+    <div class="text-grey">User name</div>
     <div class="text-grey mb-4">Додані маркери {{ counter }}</div>
     <slot name="label"></slot>
     <slot name="list"></slot>
