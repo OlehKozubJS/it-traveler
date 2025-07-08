37bdd32b6512e2cdb29c598a09799f4c39330dee
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 16:58:06 +0300

    Update FavouritePlace.vue

--- src/components/FavouritePlace/FavouritePlace.vue
+++ src/components/FavouritePlace/FavouritePlace.vue
@@ -3,7 +3,7 @@
 <template>
   <section section class="text-[#939393] mb-6 last:mb-0">
     <div class="flex gap-4">
-      <img class="" src="" alt="" />
+      <img class="w-[76px] h-[76px] shrink-0" src="" alt="" />
       <div class=""></div>
       <p class=""></p>
     </div>
