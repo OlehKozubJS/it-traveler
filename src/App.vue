f13fc0330054d11dae74057592a3eb42173d486d
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 17:07:27 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import FavouritePlace from './components/FavouritePlace/FavouritePlace.vue'
 import { HomepageView } from './views'
 </script>
 
 <template>
+  <FavouritePlace />
   <HomepageView />
 </template>
