bca8006734c238e37f070269387bb3dc5584c582
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 16:56:40 +0300

    Update FavouritePlace.vue

--- src/components/FavouritePlace/FavouritePlace.vue
+++ src/components/FavouritePlace/FavouritePlace.vue
@@ -2,7 +2,11 @@
 
 <template>
   <section section class="text-[#939393] mb-6 last:mb-0">
-    <div class="flex gap-4"></div>
+    <div class="flex gap-4">
+      <img src="" alt="" />
+      <div></div>
+      <p></p>
+    </div>
     <div class="h-[1px] w-full bg-[#ececec]"></div>
   </section>
 </template>
