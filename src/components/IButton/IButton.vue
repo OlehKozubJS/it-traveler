7e3eaeea83c63d5ae82a9096136f009562ebc714
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 20:16:48 +0300

    Update IButton.vue

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['variant'])
+</script>
+
 <template>
   <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">
     <slot></slot>
