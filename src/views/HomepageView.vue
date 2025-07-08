64438169d1a32cb50ae2e17d94fa405f13db5875
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 15:43:56 +0300

    Update HomepageView.vue

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -11,7 +11,7 @@ import { IButton } from '../components/IButton'
       <div class="text-white text-center">
         <img class="inline mb-6" src="../assets/img/map-pin.svg" alt="" />
         <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
-        <p class="leading-6 mb-11 text-blue-700">
+        <p class="leading-6 mb-11">
           Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
           ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
         </p>
