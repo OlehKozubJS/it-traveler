2f161906257cf20b94ac992784a8675dbdcb4181
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 20:28:17 +0300

    Update IButton.vue

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { defineProps } from 'vue'
 
-const props = defineProps(['variant'])
+const props = defineProps({'variant'})
 const bgStyles =
   props.variant === 'gradient' ? 'bg-gradient-to-r from-[#FFA279] to-[#F3743D]' : 'bg-[#FFA279]'
 </script>
