b8674f5c89f509c3d3ff07d0026aa8cfcbf7de7d
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 20:48:32 +0300

    Update FavouritePlaces.vue

--- src/components/FavouritePlaces/FavouritePlaces.vue
+++ src/components/FavouritePlaces/FavouritePlaces.vue
@@ -13,10 +13,12 @@ const changeButtonVariant = () => {
 
 <template>
   <div class="px-6">
-    <div class="text-grey mb-4">Додані маркери {{ counter }}</div>
+    <div class="text-grey mb-4">Додані маркери {{ variant }}</div>
     <slot name="label"></slot>
     <slot name="list"></slot>
     <FavouritePlace :key="number" v-for="number in 4" />
-    <IButton class="w-full mt-10" @click="increment" variant="gradient">Додати маркер</IButton>
+    <IButton class="w-full mt-10" @click="changeButtonVariant" variant="gradient"
+      >Додати маркер</IButton
+    >
   </div>
 </template>
