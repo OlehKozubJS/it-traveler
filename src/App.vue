03205b956e18b25820ee0c646ccc3b286d41d60b
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 19:46:57 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 import FavouritePlaces from './components/FavouritePlaces/FavouritePlaces.vue'
 import { HomepageView } from './views'
-//import { TestComponent } from '../components/TestComponent'
-//<TestComponent />
+import { TestComponent } from './components/TestComponent'
 </script>
 
 <template>
   <FavouritePlaces> </FavouritePlaces>
-
+  <TestComponent />
   <!--HomepageView /-->
 </template>
