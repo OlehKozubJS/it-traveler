6e9df082293d6cc022c1dea177c8e6f83cc426bf
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-08 21:25:02 +0300

    Update IInput.vue

--- src/components/IInput/IInput.vue
+++ src/components/IInput/IInput.vue
@@ -4,11 +4,11 @@ const props = defineProps({ label: String })
 
 <template>
   <div class="w-full text-[#2C2C2C]">
-    <label for="" class="block">
+    <label class="block" for="">
       <span class="block text-xs ps-3 mb-2">{{ props.label }}</span>
       <input
-        type="text"
         class="w-full text-sm rounded-[4px] border-[#eaeaea] border-[1px] py-2 px-3 focus:outline-primary"
+        type="text"
       />
     </label>
   </div>
