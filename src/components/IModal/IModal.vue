6ac6d10133ef9c1b0bdc1b712e6e52b58c8eb2e1
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 15:46:42 +0300

    Update IModal.vue

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -5,7 +5,9 @@ import CrossIcon from '../icons/CrossIcon.vue'
 <template>
   <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,3)]">
     <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
-      <button></button>
+      <button>
+        <CrossIcon class="absolute right-3 top-3 w-6 h-6" />
+      </button>
     </div>
   </div>
 </template>
