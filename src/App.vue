e698fe9756c4bc60e1511fb4bfadf29f304bddb2
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 15:52:05 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { ref } from 'vue'
+
 import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
 import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 import IModal from './components/IModal/IModal.vue'
 //import FavouritePlaces from './components/FavouritePlaces/FavouritePlaces.vue'
 //import { HomepageView } from './views'
 //import { TestComponent } from './components/TestComponent'
+
+const isOpen = ref()
 </script>
 
 <template>
@@ -13,5 +17,5 @@ import IModal from './components/IModal/IModal.vue'
   <!--HomepageView /-->
   <RegistrationForm @submit="console.log" />
   <LoginForm @submit="console.log" />
-  <IModal />
+  <IModal @close="console.log('closed')" />
 </template>
