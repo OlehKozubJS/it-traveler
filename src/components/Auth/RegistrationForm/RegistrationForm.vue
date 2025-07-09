2aa0566c92c4bf5767f062233447be6145eecc7f
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 11:12:04 +0300

    Update RegistrationForm.vue

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -4,7 +4,7 @@ import IButton from '@/components/IButton/IButton.vue'
 </script>
 
 <template>
-  <form action="">
+  <form action="" class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
     <IInput />
     <IInput />
     <IInput />
