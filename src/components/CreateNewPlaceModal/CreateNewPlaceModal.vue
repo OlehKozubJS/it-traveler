cc8aab187f1f8edf286d24f920548b1d6dccc260
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 19:53:58 +0300

    Update CreateNewPlaceModal.vue

--- src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
+++ src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
@@ -22,9 +22,10 @@ const handleUpload = (url) => {
       <div class="flex gap-1 font-bold justify-center mb-10"><MarkerIcon />Додати маркери</div>
       <IInput label="Локація" class="mb-4" v-model="formData.title" />
       <IInput label="Опис" type="textarea" class="mb-2" v-model="formData.description" />
-      <img v-if="formData.img" :src="formData.img" alt="" class="w-10 h-10 object-cover" />
-      <InputImage class="mb-10" @uploaded="handleUpload" />
-
+      <div class="flex gap-1 items-center mb-10">
+        <img v-if="formData.img" :src="formData.img" alt="" class="w-20 h-20 object-cover" />
+        <InputImage class="mb-10" @uploaded="handleUpload" />
+      </div>
       <IButton variant="gradient" class="w-full">Додати</IButton>
     </form>
   </IModal>
