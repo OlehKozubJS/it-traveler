d5d97f84c7e20a7e0aff2c20d073bab4ce2b3507
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 19:47:24 +0300

    Update CreateNewPlaceModal.vue

--- src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
+++ src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
@@ -18,14 +18,12 @@ const handleUpload = (url) => {
 
 <template>
   <IModal v-if="props.isOpen" @close="emit('close')">
-    <form @submit.prevent="emit('submit')" class="min-w-[420px]">
-      <div class="min-w-[420px]">
-        <div class="flex gap-1 font-bold justify-center mb-10"><MarkerIcon />Додати маркери</div>
-        <IInput label="Локація" class="mb-4" v-model="formData.title" />
-        <IInput label="Опис" type="textarea" class="mb-2" v-model="formData.description" />
-        <InputImage class="mb-10" @uploaded="handleUpload" />
-        <IButton variant="gradient" class="w-full">Додати</IButton>
-      </div>
+    <form @submit.prevent="emit('submit', formData)" class="min-w-[420px]">
+      <div class="flex gap-1 font-bold justify-center mb-10"><MarkerIcon />Додати маркери</div>
+      <IInput label="Локація" class="mb-4" v-model="formData.title" />
+      <IInput label="Опис" type="textarea" class="mb-2" v-model="formData.description" />
+      <InputImage class="mb-10" @uploaded="handleUpload" />
+      <IButton variant="gradient" class="w-full">Додати</IButton>
     </form>
   </IModal>
 </template>
