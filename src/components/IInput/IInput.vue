fa128fd9351c248d8e3f835b311d64347b214538
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 10:50:22 +0300

    Update IInput.vue

--- src/components/IInput/IInput.vue
+++ src/components/IInput/IInput.vue
@@ -6,19 +6,20 @@ const props = defineProps({ label: String })
 const text = ref('')
 
 const changeValue = (event) => {
-  text.value = event.target
+  text.value = event.target.value
 }
 </script>
 
 <template>
   <div class="w-full text-[#2C2C2C]">
+    {{ text }}
     <label class="block" for="">
       <span class="block text-xs ps-3 mb-2">{{ props.label }}</span>
       <input
         class="w-full text-sm rounded-[4px] border-[#eaeaea] border-[1px] py-2 px-3 focus:outline-primary"
         type="text"
+        @input="changeValue"
       />
     </label>
-    {{ text }}
   </div>
 </template>
