5c3032465ee8459458a293adf648c76a87b12bb0
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 16:14:47 +0300

    Update IModal.vue

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -17,7 +17,7 @@ onUnmounted(() => {
 <template>
   <div
     class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]"
-    @click="emit('close')"
+    @click.self="emit('close')"
   >
     <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
       <button @click="emit('close')">
