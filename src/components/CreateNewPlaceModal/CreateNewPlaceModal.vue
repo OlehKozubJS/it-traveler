6aceb3ecf7a408d1e3c064468b4a07829d60c84f
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 18:58:02 +0300

    Update CreateNewPlaceModal.vue

--- src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
+++ src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
@@ -1,9 +1,15 @@
 <script setup>
 import IModal from '../IModal/IModal.vue'
+import IInput from '../IInput/IInput.vue'
 
-const props = defineProps({ isOpen: Boolean })
+const props = defineProps({ isOpen: { default: false, type: Boolean } })
 </script>
 
 <template>
-  <IModal></IModal>
+  <IModal v-if="props.isOpen">
+    <div>
+      <IInput />
+      <IInput type="textarea" />
+    </div>
+  </IModal>
 </template>
