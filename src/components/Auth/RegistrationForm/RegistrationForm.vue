8540888ea0e61abd9a892356b6cd2a666e1156e4
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 11:27:54 +0300

    Update RegistrationForm.vue

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -7,7 +7,7 @@ import IButton from '@/components/IButton/IButton.vue'
   <form action="" class="max-w-[500px] w-full bg-white p-10 rounded-2xl">
     <IInput class="mb-4" label="Повне ім'я" />
     <IInput class="mb-4" label="Електронна пошта" type="email" />
-    <IInput class="mb-4" label="Пароль" type="password" />
+    <IInput class="mb-4" label="Пароль" type="password" disabled />
     <IButton variant="gradient">Створити акаунт</IButton>
   </form>
 </template>
