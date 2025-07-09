0cc668f8deb9bf8509b80e7c3aa12924df4e6e5a
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 16:10:14 +0300

    Update IModal.vue

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -8,10 +8,14 @@ const emit = defineEmits(['close'])
 onMounted(() => {
   document.body.style.overflow = 'hidden'
 })
+
+onUnmounted(() => {
+  document.body.style.overflow = 'initial'
+})
 </script>
 
 <template>
-  <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,3)]">
+  <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]">
     <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
       <button @click="emit('close')">
         <CrossIcon class="absolute right-3 top-3 w-6 h-6" />
