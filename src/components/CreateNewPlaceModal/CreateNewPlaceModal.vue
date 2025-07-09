6dac9c2a153fa815161ae534c4e47693c1e502cd
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 19:45:17 +0300

    Update CreateNewPlaceModal.vue

--- src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
+++ src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
@@ -10,16 +10,20 @@ import { reactive } from 'vue'
 
 const emit = defineEmits(['close', 'submit'])
 const formData = reactive({ title: '', description: '', img: '' })
+
+const handleUpload = () => {}
 </script>
 
 <template>
   <IModal v-if="props.isOpen" @close="emit('close')">
-    <div class="min-w-[420px]">
-      <div class="flex gap-1 font-bold justify-center mb-10"><MarkerIcon />Додати маркери</div>
-      <IInput label="Локація" class="mb-4" />
-      <IInput label="Опис" type="textarea" class="mb-2" />
-      <InputImage class="mb-10" />
-      <IButton variant="gradient" class="w-full">Додати</IButton>
-    </div>
+    <form @submit.prevent="emit('submit')" class="min-w-[420px]">
+      <div class="min-w-[420px]">
+        <div class="flex gap-1 font-bold justify-center mb-10"><MarkerIcon />Додати маркери</div>
+        <IInput label="Локація" class="mb-4" v-model="formData.title" />
+        <IInput label="Опис" type="textarea" class="mb-2" v-model="formData.description" />
+        <InputImage class="mb-10" />
+        <IButton variant="gradient" class="w-full">Додати</IButton>
+      </div>
+    </form>
   </IModal>
 </template>
