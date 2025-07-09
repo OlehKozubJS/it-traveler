f764eafc1b9ebb24e002f25d89439dd1693a29e9
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 11:09:28 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
+
 //import FavouritePlaces from './components/FavouritePlaces/FavouritePlaces.vue'
 //import { HomepageView } from './views'
 //import { TestComponent } from './components/TestComponent'
