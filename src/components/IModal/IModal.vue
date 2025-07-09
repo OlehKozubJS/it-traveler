f4b2855465921f9443a6e46e6d76a1834e2fe155
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 16:13:47 +0300

    Update IModal.vue

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -15,7 +15,10 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]">
+  <div
+    class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]"
+    @click="emit('close')"
+  >
     <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
       <button @click="emit('close')">
         <CrossIcon class="absolute right-3 top-3 w-6 h-6" />
