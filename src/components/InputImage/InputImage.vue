b19a78e34a3cdb88c8cf2d1896ae749ecf5770cb
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 16:55:55 +0300

    Update InputImage.vue

--- src/components/InputImage/InputImage.vue
+++ src/components/InputImage/InputImage.vue
@@ -4,7 +4,7 @@ import UploadIcon from './UploadIcon.vue'
 
 <template>
   <label classr="cursor-pointer hover:text-primary">
-    <input type="file" accept="image/*" class="hidden" />
+    <input type="file" accept="image/*" class="hidden" @change="handleUploadImage()" />
     <span class="flex gap-1 items-center">
       <UploadIcon />
       <span class="underline text-xs">Натисність тут, щоб додати фото</span>
