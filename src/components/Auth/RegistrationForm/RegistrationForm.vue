cd667c919462b7be6f49d14f70f3b4ab1f7e95f6
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 11:41:07 +0300

    Update RegistrationForm.vue

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -6,7 +6,11 @@ const emit = defineEmits(['submit'])
 </script>
 
 <template>
-  <form action="" class="max-w-[500px] w-full bg-white p-10 rounded-2xl" @submit="emit('submit')">
+  <form
+    action=""
+    class="max-w-[500px] w-full bg-white p-10 rounded-2xl"
+    @submit.prevent="emit('submit')"
+  >
     <IInput class="mb-4" label="Повне ім'я" />
     <IInput class="mb-4" label="Електронна пошта" type="email" />
     <IInput class="mb-4" label="Пароль" type="password" />
