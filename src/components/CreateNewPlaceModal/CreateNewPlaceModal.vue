86714503ddd8bc313b92d57a8c1cf2c504843b73
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 19:26:03 +0300

    Update CreateNewPlaceModal.vue

--- src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
+++ src/components/CreateNewPlaceModal/CreateNewPlaceModal.vue
@@ -13,8 +13,8 @@ const emit = defineEmits(['close'])
   <IModal v-if="props.isOpen" @close="emit('close')">
     <div class="min-w-[420px]">
       <IInput label="Локація" class="mb-4" />
-      <IInput label="Опис" type="textarea" class="mb-3" />
-      <InputImage />
+      <IInput label="Опис" type="textarea" class="mb-2" />
+      <InputImage class="mb-10" />
       <IButton variant="gradient">Додати</IButton>
     </div>
   </IModal>
