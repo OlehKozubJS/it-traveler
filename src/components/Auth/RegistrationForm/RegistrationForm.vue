c74475424729acbafc5d50e262d961a221f9cb9f
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 11:06:20 +0300

    Update RegistrationForm.vue

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -1,3 +1,13 @@
-<script setup></script>
+<script setup>
+import IInput from '@/components/IInput/IInput.vue'
+import IButton from '@/components/IButton/IButton.vue'
+</script>
 
-<template></template>
+<template>
+  <form action="">
+    <IInput />
+    <IInput />
+    <IInput />
+    <IButton />
+  </form>
+</template>
