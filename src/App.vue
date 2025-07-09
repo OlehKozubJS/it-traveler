f797b85be604b99ef41722e1366eee71ea91c431
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 20:59:47 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { MapboxMap } from '@studiometa/vue-mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 import { ref } from 'vue'
 
 import FavouritePlaces from './components/FavouritePlaces/FavouritePlaces.vue'
@@ -19,6 +22,8 @@ const openModal = () => {
     <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
       <FavouritePlaces />
     </div>
-    <div class="w-full h-full flex items-center justify-center text-6xl"></div>
+    <div class="w-full h-full flex items-center justify-center text-6xl">
+      <MapboxMap class="w-full h-full"></MapboxMap>
+    </div>
   </main>
 </template>
