2f8eb2ac9c2a161fae888d00d6203abab9d072d3
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 16:43:34 +0300

    Update InputImage.vue

---
+++ src/components/InputImage/InputImage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import UploadIcon from './UploadIcon.vue'
+</script>
+
+<template>
+  <label classr="cursor-pointer hover-text-primary">
+    <span>
+      <input type="text" />
+      <span></span>
+    </span>
+  </label>
+  <span></span>
+</template>
