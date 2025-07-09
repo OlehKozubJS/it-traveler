2a421cd25f27955b5fa07069a615b57e9c490b6c
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 12:32:44 +0300

    Update LoginForm.vue

--- src/components/Auth/LoginForm/LoginForm.vue
+++ src/components/Auth/LoginForm/LoginForm.vue
@@ -12,7 +12,6 @@ const userData = reactive({ name: '', email: '', password: '' })
 
 <template>
   <FormContainer @submit.prevent="emit('submit', toRaw(userData))">
-    <IInput class="mb-4" label="Повне ім'я" v-model="userData.name" />
     <IInput class="mb-4" label="Електронна пошта" type="email" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" v-model="userData.password" />
     <IButton variant="gradient" type="submit" class="mt-8 w-full">Створити акаунт</IButton>
