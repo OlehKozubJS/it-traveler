0a9a93ecdc901f447ce0c6506dce94b09010248e
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 16:33:04 +0300

    Update IModal.vue

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -15,15 +15,16 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div
-    class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]"
-    @click.self="emit('close')"
-  >
-    <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
-      <button @click="emit('close')">
-        <CrossIcon class="absolute right-3 top-3 w-6 h-6" />
-      </button>
-      <slot></slot>
-    </div>
-  </div>
+  <component :is="Teleport" to="" body>
+    <div
+      class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,0.3)]"
+      @click.self="emit('close')"
+    >
+      <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
+        <button @click="emit('close')">
+          <CrossIcon class="absolute right-3 top-3 w-6 h-6" />
+        </button>
+        <slot></slot>
+      </div></div
+  ></component>
 </template>
