52bd0e55a87881ef9b9494c9bb1d2f7148a73e36
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 23:30:58 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ const favouritePlaces = [
 <template>
   <main class="flex h-screen">
     <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
-      <FavouritePlaces />
+      <FavouritePlaces :items="favouritePlaces" />
     </div>
     <div class="w-full h-full flex items-center justify-center text-6xl">
       <MapboxMap
