f8421195038a2196d5c2cbb265653f6c5d912d2a
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 15:45:46 +0300

    Update IModal.vue

--- src/components/IModal/IModal.vue
+++ src/components/IModal/IModal.vue
@@ -1,3 +1,11 @@
 <script setup>
-//import CrossIcon from './icons/CrossIcon.vue;
+import CrossIcon from '../icons/CrossIcon.vue'
 </script>
+
+<template>
+  <div class="flex w-full h-full fixed top-0 left-0 overflow-auto bg-[rgba(0,0,0,3)]">
+    <div class="relative bg-white min-w-[350px] m-auto text-black rounded-2xl p-10">
+      <button></button>
+    </div>
+  </div>
+</template>
