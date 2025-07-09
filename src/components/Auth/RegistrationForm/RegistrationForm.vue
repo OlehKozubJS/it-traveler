bc1dfc0d9ec54b1e2dc4924c86f2e4bfe4f38f5d
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 12:12:50 +0300

    Update RegistrationForm.vue

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -6,7 +6,7 @@ import IButton from '@/components/IButton/IButton.vue'
 
 const emit = defineEmits(['submit'])
 
-const userData = reactive('')
+const userData = reactive({ name: '', email: '', password: '' })
 </script>
 
 <template>
@@ -15,10 +15,11 @@ const userData = reactive('')
     class="max-w-[500px] w-full bg-white p-10 rounded-2xl"
     @submit.prevent="emit('submit', foo)"
   >
-    <input type="text" class="text-black" v-model="foo" />
-    <IInput class="mb-4" label="Повне ім'я" v-model="foo" />
-    <IInput class="mb-4" label="Електронна пошта" type="email" />
+    <IInput class="mb-4" label="Повне ім'я" v-model="userData.name" />
+    <IInput class="mb-4" label="Електронна пошта" type="email" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" />
-    <IButton variant="gradient" type="submit" class="mt-8 w-full">Створити акаунт</IButton>
+    <IButton variant="gradient" type="submit" class="mt-8 w-full" v-model="userData.password"
+      >Створити акаунт</IButton
+    >
   </form>
 </template>
