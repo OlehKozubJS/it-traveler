b9667f4f826a463d64e7c5e7208eb5cf91adb976
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 10:40:39 +0300

    Update FavouritePlaces.vue

--- src/components/FavouritePlaces/FavouritePlaces.vue
+++ src/components/FavouritePlaces/FavouritePlaces.vue
@@ -10,11 +10,14 @@ const buttonVariant = ref('gradient')
 const changeButtonVariant = () => {
   buttonVariant.value = buttonVariant.value === 'gradient' ? 'outlined' : 'gradient'
 }
+
+const value = ref('')
 </script>
 
 <template>
   <div class="px-6">
     <IInput label="Some label" />
+    {{ value }}
     <a
       href=""
       class="text-black"
@@ -25,7 +28,9 @@ const changeButtonVariant = () => {
       "
       >Click me</a
     >
-    <div class="text-grey mb-4">Додані маркери {{ buttonVariant }}</div>
+    <div @keydown.alt="() => console.log('alt pressed')" class="text-grey mb-4">
+      Додані маркери {{ buttonVariant }}
+    </div>
     <FavouritePlace :key="number" v-for="number in 4" />
     <IButton class="w-full mt-10" @click="changeButtonVariant" :variant="buttonVariant"
       >Додати маркер</IButton
