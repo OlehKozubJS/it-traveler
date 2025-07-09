18ab76943971c161a5e868af63e373e382b757e5
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-09 15:29:22 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-import { onMounted } from 'vue'
-
 import RegistrationForm from './components/Auth/RegistrationForm/RegistrationForm.vue'
 import LoginForm from './components/Auth/LoginForm/LoginForm.vue'
 //import FavouritePlaces from './components/FavouritePlaces/FavouritePlaces.vue'
 //import { HomepageView } from './views'
 //import { TestComponent } from './components/TestComponent'
-
-onMounted(() => {
-  console.log('mounted')
-})
 </script>
 
 <template>
