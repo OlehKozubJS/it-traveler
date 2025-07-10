20343d4471ea9355df9d22cd1611673a0dd22719
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 18:58:11 +0300

    Update RegistrationView.vue

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,10 +1,14 @@
 <script setup>
 import RegistrationForm from '../components/Auth/RegistrationForm/RegistrationForm.vue'
 import { registerUser } from '../api/user'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const handleRegisterUser = async (userData) => {
   try {
     await registerUser(userData)
+    router.replace('/map')
   } catch (error) {
     console.error(error)
   }
@@ -12,5 +16,5 @@ const handleRegisterUser = async (userData) => {
 </script>
 
 <template>
-  <RegistrationForm @submit="console.log" />
+  <RegistrationForm @submit="handleRegisterUser" />
 </template>
