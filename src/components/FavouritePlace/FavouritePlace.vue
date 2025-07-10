67bd7fe2d766a3d6a5c611daa7ef1e9fe0b8bb6a
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 09:57:01 +0300

    Update FavouritePlace.vue

--- src/components/FavouritePlace/FavouritePlace.vue
+++ src/components/FavouritePlace/FavouritePlace.vue
@@ -18,8 +18,12 @@ const props = defineProps({
         <div class="flex justify-between items-center mb-2">
           <h2 class="font-bold text-sm text-[#2C2C2C]">{{ props.title }}</h2>
           <div class="flex gap-2">
-            <FavouritePlaceIconButton><EditIcon /></FavouritePlaceIconButton>
-            <FavouritePlaceIconButton><DeleteIcon /></FavouritePlaceIconButton>
+            <FavouritePlaceIconButton>
+              <EditIcon />
+            </FavouritePlaceIconButton>
+            <FavouritePlaceIconButton>
+              <DeleteIcon />
+            </FavouritePlaceIconButton>
           </div>
         </div>
         <p class="text-xs line-clamp-3">
