8bec9c3698a7b17f4d43f73fc9052286202e96fa
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 20:09:55 +0300

    Update RegistrationView.vue

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,29 +1,25 @@
 <script setup>
-import { ref } from 'vue'
-
 import { registerUser } from '../api/user'
 import { useRouter } from 'vue-router'
 
+import { useMutation } from '@/composables/useMutation'
+
 import RegistrationForm from '../components/Auth/RegistrationForm/RegistrationForm.vue'
 
-const isLoading = ref(false)
-const router = useRouter()
-const error = ref(null)
-
-const handleRegisterUser = async (userData) => {
-  isLoading.value = true
-
-  try {
-    await registerUser(userData)
+const {
+  isLoading,
+  error,
+  mutation: handleRegisterUser,
+} = useMutation({
+  mutationFunction: registerUser,
+  onSuccess: () => {
     router.replace('/map')
-  } catch (error) {
-    console.error(error)
-  } finally {
-    isLoading.value = false
-  }
-}
+  },
+})
+const router = useRouter()
 </script>
 
 <template>
   <RegistrationForm @submit="handleRegisterUser" :is-loading="isLoading" />
+  <div v-if="error" class="text-red-500">{{ error.message }}</div>
 </template>
