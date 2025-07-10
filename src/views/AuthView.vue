22fd60acc2a10972c8f8ea4b9392b9a73e9e47bf
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 13:39:15 +0300

    Update AuthView.vue

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,7 +1,8 @@
 <script setup>
+import { RouterView } from 'vue-router'
 import BaseLayout from '../layouts/BaseLayout.vue'
 </script>
 
 <template>
-  <BaseLayout> </BaseLayout>
+  <BaseLayout> <router-view></router-view></BaseLayout>
 </template>
