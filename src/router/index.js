79283007a6927b240bdc74b1f485c56053d05d3f
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 12:36:10 +0300

    Update index.js

---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomepageView from './views/HomepageView.vue'
+import GreetingView from './views/GreetingView.vue'
+import AuthView from './views/AuthView.vue'
+
+const routes = [{ path: '/', component: HomepageView }]
