4a78bb00d3cbc1e2a576fd989e3c651334afb96a
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 12:49:29 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script setup>
-import HomepageView from './views/HomepageView.vue'
-import GreetingView from './views/GreetingView.vue'
-import AuthView from './views/AuthView.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <HomepageView /> -->
-  <!--- <GreetingView /> -->
-  <!---->
-  <AuthView />
+  <RouterView />
 </template>
