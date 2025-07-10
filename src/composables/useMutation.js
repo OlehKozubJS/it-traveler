620b67b5ee4a626f8d8f06e36de21c99d0b8ce20
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 19:44:56 +0300

    Update useMutation.js

--- src/composables/useMutation.js
+++ src/composables/useMutation.js
@@ -1,8 +1,20 @@
 import { ref } from 'vue'
 
-export const useMutation = () => {
+export const useMutation = (mutationFunction) => {
   const data = ref()
   const isLoading = ref(false)
   //const router = useRouter()
   const error = ref(null)
+
+  const mutation = async (...args) => {
+    isLoading.value = true
+
+    try {
+      await mutationFunction(...args)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      isLoading.value = false
+    }
+  }
 }
