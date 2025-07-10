e477c5565fb8610bc161b0fad0d102fb2232c2bb
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 12:30:55 +0300

    Update AuthView.vue

---
+++ src/views/AuthView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { IButton } from '../components/IButton'
+import BaseLayout from '../layouts/BaseLayout.vue'
+</script>
+
+<template>
+  <BaseLayout> </BaseLayout>
+</template>
