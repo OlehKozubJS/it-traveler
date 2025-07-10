aedf3cc9fb11ccff5d142c3834ba3236de442f70
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 18:30:27 +0300

    Update index.js

--- src/api/favourite-places/index.js
+++ src/api/favourite-places/index.js
@@ -5,3 +5,7 @@ const BASE_PLACES_URL = '/points'
 export const getFavoritePlaces = () => {
   return clientFetch.get(BASE_PLACES_URL)
 }
+
+export const getFavoritePlaces = () => {
+  return clientFetch.get(BASE_PLACES_URL)
+}
