ba483e677ef2e57884508eb568a57dbcb51b04d9
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 08:26:47 +0300

    Update FavouritePlaces.vue

--- src/components/FavouritePlaces/FavouritePlaces.vue
+++ src/components/FavouritePlaces/FavouritePlaces.vue
@@ -3,14 +3,14 @@ import FavouritePlace from '../FavouritePlace/FavouritePlace.vue'
 import IButton from '../IButton/IButton.vue'
 
 const props = defineProps({ items: { required: true, type: Array } })
+
+const addMarker = () => {}
 </script>
 
 <template>
   <div class="px-6">
-    <div class="text-grey mb-4">Додані маркери {{ buttonVariant }}</div>
+    <div class="text-grey mb-4">Додані маркери</div>
     <FavouritePlace :key="place.id" v-for="place in props.items" />
-    <IButton class="w-full mt-10" @click="changeButtonVariant" variant="gradient"
-      >Додати маркер</IButton
-    >
+    <IButton class="w-full mt-10" @click="addMarker" variant="gradient">Додати маркер</IButton>
   </div>
 </template>
