693521a3c171d42ebe5bbca458a917c37d3c5dd6
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 10:42:14 +0300

    Update FavouritePlace.vue

--- src/components/FavouritePlace/FavouritePlace.vue
+++ src/components/FavouritePlace/FavouritePlace.vue
@@ -32,6 +32,9 @@ const props = defineProps({
         </p>
       </div>
     </div>
-    <div class="h-[1px] w-full bg-[#ececec] mt-4"></div>
+    <div
+      class="h-[1px] w-full bg-[#ececec] mt-4"
+      :class="{ 'bg-primary': props.isActive, 'bg-[#ececec]': !props.isActive }"
+    ></div>
   </section>
 </template>
