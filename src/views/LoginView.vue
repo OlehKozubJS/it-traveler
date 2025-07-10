bbd25bf532f64e40f193e6fd3164ffdd061fd266
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 19:51:43 +0300

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,23 +8,11 @@ import { useMutation } from '@/composables/useMutation'
 
 import LoginForm from '../components/Auth/LoginForm/LoginForm.vue'
 
-const { data, isLoading, error, mutation } = useMutation
+const { data, isLoading, error, mutation: handleLoginUser } = useMutation(loginUser)
 const router = useRouter()
-
-const handleLoginUser = async (userData) => {
-  isLoading.value = true
-
-  try {
-    await loginUser(userData)
-    router.replace('/map')
-  } catch (error) {
-    console.error(error)
-  } finally {
-    isLoading.value = false
-  }
-}
 </script>
 
 <template>
   <LoginForm @submit="handleLoginUser" :is-loading="isLoading" />
+  <div>{{ error.message }}</div>
 </template>
