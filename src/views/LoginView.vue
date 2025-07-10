e7daec52216cb0649fc1550afdb73e3a9fb19950
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 19:49:15 +0300

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,11 +4,12 @@ import { ref } from 'vue'
 import { loginUser } from '@/api/user'
 import { useRouter } from 'vue-router'
 
+import { useMutation } from '@/composables/useMutation'
+
 import LoginForm from '../components/Auth/LoginForm/LoginForm.vue'
 
-const isLoading = ref(false)
+const { data, isLoading, error, mutation } = useMutation
 const router = useRouter()
-const error = ref(null)
 
 const handleLoginUser = async (userData) => {
   isLoading.value = true
