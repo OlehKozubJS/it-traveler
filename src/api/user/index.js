304473c95ae68c6e9415e747018b085020b51dbf
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 18:10:05 +0300

    Update index.js

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -7,3 +7,11 @@ export const login = (body) => {
 export const registerUser = (body) => {
   return clientFetch.post('/user/register', body)
 }
+
+export const logout = (body) => {
+  return clientFetch.get('/user/logout')
+}
+
+export const refresh = (body) => {
+  return clientFetch.get('/user/refresh')
+}
