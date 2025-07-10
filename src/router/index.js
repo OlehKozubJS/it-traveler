a1a954662cf3dae4295f54357cd460370da6caea
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 13:12:44 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,22 @@ import HomepageView from '../views/HomepageView.vue'
 import GreetingView from '../views/GreetingView.vue'
 import AuthView from '../views/AuthView.vue'
 
-const greetingPage = () => {
+const GreetingPage = () => {
+  import('../views/GreetingView.vue')
+}
+
+const HomePage = () => {
   import('../views/HomepageView.vue')
 }
 
+const AuthPage = () => {
+  import('../views/AuthView.vue')
+}
+
 const routes = [
-  { path: '/', component: GreetingView },
-  { path: '/map', component: HomepageView },
-  { path: '/auth', component: AuthView },
+  { path: '/', component: GreetingPage },
+  { path: '/map', component: HomePage },
+  { path: '/auth', component: AuthPage },
 ]
 
 export const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes })
