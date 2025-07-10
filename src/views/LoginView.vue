cacb852d6e6bf6a690ba8835c05354e2d128f8f9
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 19:26:53 +0300

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import LoginForm from '../components/Auth/LoginForm/LoginForm.vue'
 
-import { login } from '@/api/user'
+import { loginUser } from '@/api/user'
 </script>
 
 <template>
