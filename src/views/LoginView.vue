45579923a8078134fee51a3539b7dba4473234a7
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 19:29:01 +0300

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,28 @@
 <script setup>
-import LoginForm from '../components/Auth/LoginForm/LoginForm.vue'
+import { ref } from 'vue'
 
 import { loginUser } from '@/api/user'
+import { useRouter } from 'vue-router'
+
+import LoginForm from '../components/Auth/LoginForm/LoginForm.vue'
+
+const router = useRouter()
+const isLoading = ref(false)
+
+const handleLoginUser = async (userData) => {
+  isLoading.value = true
+
+  try {
+    await loginUser(userData)
+    router.replace('/map')
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isLoading.value = false
+  }
+}
 </script>
 
 <template>
-  <LoginForm />
+  <LoginForm @submit="handleRegisterUser" :is-loading="isLoading" />
 </template>
