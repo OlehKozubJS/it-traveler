b159a5e183320a35b2ff2a857f2114769395079b
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 09:55:17 +0300

    Update FavouritePlace.vue

--- src/components/FavouritePlace/FavouritePlace.vue
+++ src/components/FavouritePlace/FavouritePlace.vue
@@ -14,7 +14,7 @@ const props = defineProps({
   <section section class="text-grey mb-6 last:mb-0">
     <div class="flex gap-4">
       <img class="w-[76px] h-[76px] shrink-0" :src="props.img" alt="" />
-      <div>
+      <div class="w-full">
         <div class="flex justify-between items-center mb-2">
           <h2 class="font-bold text-sm text-[#2C2C2C]">{{ props.title }}</h2>
           <div class="flex gap-2">
