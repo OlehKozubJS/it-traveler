423dc272752c4cffe9e3ead08127dd502779da73
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 19:17:22 +0300

    Update RegistrationForm.vue

--- src/components/Auth/RegistrationForm/RegistrationForm.vue
+++ src/components/Auth/RegistrationForm/RegistrationForm.vue
@@ -16,6 +16,8 @@ const userData = reactive({ name: '', email: '', password: '' })
     <IInput class="mb-4" label="Повне ім'я" v-model="userData.name" />
     <IInput class="mb-4" label="Електронна пошта" type="email" v-model="userData.email" />
     <IInput class="mb-4" label="Пароль" type="password" v-model="userData.password" />
-    <IButton variant="gradient" type="submit" class="mt-8 w-full">Створити акаунт</IButton>
+    <IButton variant="gradient" type="submit" class="mt-8 w-full" :isLoading="props.isLoading">
+      Створити акаунт
+    </IButton>
   </form>
 </template>
