d4ad7207180f9423f84c4f0d8e8aeae9dc6ea03c
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 18:42:54 +0300

    Update index.js

--- src/api/favourite-places/index.js
+++ src/api/favourite-places/index.js
@@ -6,6 +6,14 @@ export const getFavoritePlaces = () => {
   return clientFetch.get(BASE_PLACES_URL)
 }
 
-export const addFavoritePlace = () => {
-  return clientFetch.post(BASE_PLACES_URL)
+export const addFavoritePlace = (body) => {
+  return clientFetch.post(BASE_PLACES_URL, body)
+}
+
+export const updateFavoritePlace = (body) => {
+  return clientFetch.put(BASE_PLACES_URL, body)
+}
+
+export const deleteFavoritePlace = (body) => {
+  return clientFetch.put(BASE_PLACES_URL, body)
 }
