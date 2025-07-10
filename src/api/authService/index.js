eb61c9b53e1c016c26be876499fd02302c6892d5
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-10 20:22:36 +0300

    Update index.js

--- src/api/authService/index.js
+++ src/api/authService/index.js
@@ -4,4 +4,22 @@ class AuthService {
   isLoggedIn() {
     return Boolean(this.#token)
   }
+
+  setToken() {}
+
+  async loginUser(body) {
+    return clientFetch.post('/user/login', body)
+  }
+
+  async registerUser(body) {
+    return clientFetch.post('/user/register', body)
+  }
+
+  async logout() {
+    return clientFetch.get('/user/logout')
+  }
+
+  async refresh() {
+    return clientFetch.get('/user/refresh')
+  }
 }
