d93de994ec46ad9e14015a6b6a7d88d7e3bf2af7
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 00:20:04 +0300

    Update HomepageView.vue

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -113,10 +113,15 @@ onMounted(() => {
           anchor="bottom"
         >
           <button @click="changeActiveId(place._id)">
-            <template v-if="place._id === activeId">
+            <template v-if="place._id !== activeId">
               <MarkerIcon :class="place._id === activeId ? 'h-10 w-10' : 'h-8 w-8'" />
             </template>
-            <template v-else>
+          </button>
+        </MapboxMarker>
+
+        <MapboxMarker :lngLat="place.coordinates" anchor="bottom">
+          <button @click="changeActiveId(place._id)">
+            <template v-if="place._id === activeId">
               <ChosenMarkerIcon :class="place._id === activeId ? 'h-10 w-10' : 'h-8 w-8'" />
             </template>
           </button>
