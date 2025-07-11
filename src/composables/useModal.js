ff52402cd5031a8e2760f790df8c440119274e5d
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-11 18:14:23 +0300

    Update useModal.js

---
+++ src/composables/useModal.js
@@ -0,0 +1,13 @@
+import { ref } from 'vue'
+
+export const useModal = () => {
+  const isOpen = ref(false)
+
+  const openModal = () => {
+    isOpen.value = true
+  }
+
+  const closeModal = () => {
+    isOpen.value = false
+  }
+}
