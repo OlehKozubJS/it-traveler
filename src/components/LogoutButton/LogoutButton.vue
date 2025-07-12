2b6310d8f3a4cd4ee27433ca4e5fb983b1e23c3f
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 21:08:42 +0300

    Update LogoutButton.vue

--- src/components/LogoutButton/LogoutButton.vue
+++ src/components/LogoutButton/LogoutButton.vue
@@ -1,3 +1,7 @@
-<script setup></script>
+<script setup>
+import LogoutIcon from './LogoutIcon.vue'
+</script>
 
-<template></template>
+<template>
+  <button class="flex gap-2 items-center px-6 text-black">Вихід<LogoutIcon /></button>
+</template>
