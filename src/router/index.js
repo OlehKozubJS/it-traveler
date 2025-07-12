4f83c04e58692098f630e99fcd3e536a4200717b
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 21:51:34 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -21,4 +21,9 @@ const routes = [
   },
 ]
 
-export const router = createRouter({ history: createWebHistory(import.meta.env.BASE_URL), routes })
+export const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+router.beforeEach
