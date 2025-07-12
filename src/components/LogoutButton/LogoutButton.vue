9d33470bfe3fb63e5487937cddc797a7a0ef812a
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 21:35:59 +0300

    Update LogoutButton.vue

--- src/components/LogoutButton/LogoutButton.vue
+++ src/components/LogoutButton/LogoutButton.vue
@@ -1,7 +1,16 @@
 <script setup>
+import { logout } from '../../api/user'
+import { useMutation } from '../../composables/useMutation'
+
 import LogoutIcon from './LogoutIcon.vue'
+
+const handlelogout = () => {
+  logout()
+}
 </script>
 
 <template>
-  <button class="flex gap-2 items-center px-6 text-black">Вихід<LogoutIcon /></button>
+  <button class="flex gap-2 items-center px-6 text-black" @click="handlelogout">
+    Вихід<LogoutIcon />
+  </button>
 </template>
