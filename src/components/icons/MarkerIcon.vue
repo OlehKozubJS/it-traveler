7d10d1dd51f00741b66ec368d7572c960667d6b7
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 16:08:46 +0300

    Update MarkerIcon.vue

--- src/components/icons/MarkerIcon.vue
+++ src/components/icons/MarkerIcon.vue
@@ -1,3 +1,7 @@
+<script setup>
+const props = defineProps({ isActive: { type: Boolean, default: false } })
+</script>
+
 <template>
   <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
