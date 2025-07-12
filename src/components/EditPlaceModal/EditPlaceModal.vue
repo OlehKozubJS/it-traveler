7403786ae098863a6a40463786c170ed4b82bdb2
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 11:26:00 +0300

    Update EditPlaceModal.vue

---
+++ src/components/EditPlaceModal/EditPlaceModal.vue
@@ -0,0 +1,7 @@
+<script setup>
+import IButton from '../IButton/IButton.vue'
+import IInput from '../IInput/IInput.vue'
+import IModal from '../IModal/IModal.vue'
+import InputImage from '../InputImage/InputImage.vue'
+import MarkerIcon from '../icons/MarkerIcon.vue'
+</script>
