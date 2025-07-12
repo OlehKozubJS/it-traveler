4ebb853e2bb00fb0bdedcd051d6be32bfcb6fb13
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 21:25:05 +0300

    Update HomepageView.vue

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -78,6 +78,7 @@ onMounted(() => {
   <main class="flex h-screen">
     <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
       <div v-if="isPlacesLoading" class="text-black px-6">Loading...</div>
+      <UserInfo />
       <FavouritePlaces
         :items="favouritePlaces"
         :active-id="activeId"
@@ -93,6 +94,7 @@ onMounted(() => {
         @close="closeModal"
         @submit="handleAddPlace"
       />
+      <LogoutButton />
     </div>
     <div class="w-full h-full flex items-center justify-center text-6xl">
       <MapboxMap
