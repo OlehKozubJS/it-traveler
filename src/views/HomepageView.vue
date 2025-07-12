158222ffe5255bbb85c5f905317a1ddd6deb4a0a
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 20:20:21 +0300

    Update HomepageView.vue

--- src/views/HomepageView.vue
+++ src/views/HomepageView.vue
@@ -75,7 +75,7 @@ onMounted(() => {
 <template>
   <main class="flex h-screen">
     <div class="bg-white h-full w-[400px] shrink-0 overflow-auto pb-10">
-      <div v-if="isPlacesLoading" class="text-black">Loading...</div>
+      <div v-if="isPlacesLoading" class="text-black px-6">Loading...</div>
       <FavouritePlaces
         :items="favouritePlaces"
         :active-id="activeId"
