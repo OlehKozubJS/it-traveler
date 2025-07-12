c0892dc527ee273c75ad281439347cffe32ce196
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-12 18:50:50 +0300

    Update ConfirmationModal.vue

--- src/components/ConfirmationModal/ConfirmationModal.vue
+++ src/components/ConfirmationModal/ConfirmationModal.vue
@@ -1,3 +1,21 @@
-<script setup></script>
+<script setup>
+import IButton from '../IButton/IButton.vue'
+import IModal from '../IModal/IModal.vue'
+
+const props = defineProps({
+  title: {
+    default: false,
+    type: String,
+  },
+  isOpen: {
+    default: false,
+    type: Boolean,
+  },
+  isLoading: {
+    default: false,
+    type: Boolean,
+  },
+})
+</script>
 
 <template></template>
