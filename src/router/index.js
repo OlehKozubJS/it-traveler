dff93bbef44062fd13a6befb160f0c67f0dbcf29
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-13 13:43:16 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { authService } from '../api/authService'
-/*
+
 const GreetingPage = () => import('../views/GreetingView.vue')
 const HomePage = () => import('../views/HomepageView.vue')
 const AuthPage = () => import('../views/AuthView.vue')
 const LoginPage = () => import('../views/LoginView.vue')
 const RegistrationPage = () => import('../views/RegistrationView.vue')
-*/
 
 const routes = [
   { path: '/', component: GreetingPage, name: 'greeting' },
