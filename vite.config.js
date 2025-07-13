e07a79d06d05ff4f04b29172b368975d903621b1
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-13 14:43:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: { dynamicImportVars: true },
   base: '/it-traveler/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
