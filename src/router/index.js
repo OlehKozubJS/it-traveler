ab7f36a185532ee251727cd919347583029bf178
OlehKozubJS <oleg.kozub54@gmail.com>
2025-07-13 14:43:53 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,7 @@ export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-/*
+
 router.onError((error, to) => {
   if (
     error.message.includes('Failed to fetch dynamically imported module') ||
@@ -41,7 +41,7 @@ router.onError((error, to) => {
     window.location = to.fullPath
   }
 })
-*/
+
 router.beforeEach((to, from, next) => {
   const authRoutes = ['login', 'registration']
   const { name } = to
